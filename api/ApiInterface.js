227a581949932ad4bacd76fba955d8962341b245
Harish Gyanani <gyanani.harish@gmail.com>
2019-04-19 17:07:11 +0530

    Add files via upload

---
+++ api/ApiInterface.js
@@ -0,0 +1,14 @@
+//export const BASE_API_URL = 'http://npfonlinetest.com/KioskMobApp/api/Kiosk/'
+export const BASE_API_URL = 'http://npfonline.com/KioskMobApp/api/Kiosk/'
+
+export const RESPONSE_TIMEOUT = 60000
+
+export const COMMON_HEADERS = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/x-www-form-urlencoded'
+}
+
+export const WEBAPI_GET_FEED = {
+    'type': 'GET',
+    'endpoint': '/feeds'
+}
---
+++ api/WebApiStatus.js
@@ -0,0 +1,19 @@
+export const WEB_API_STATUS_INVALID_LOGIN=1013
+export const WEB_API_STATUS_SESSION_EXPIRED=1014
+export const INTERNAL_SERVER_ERROR=500;
+/* Status Code	Status Message
+1001	Invalid UserName or Password
+1002	Database is offline
+1003	Please contact System Admin
+1004	Password expired. Please contact Admin
+1005	Invalid UserName or Password
+1006	Invalid UserName or Password
+1007	Invalid User or email id
+1008	Request not processed due to unregistered email id. Please contact Admin
+1009	Maximum number of attempts reached for the day. Please retry tomorrow
+1010	Invalid Data. Please Contact System Admin
+1011	Invalid Client Detail. Please Contact System Admin
+1012	Invalid Menu. Please Contact System Admin
+1013	Sorry! It is not a valid Login.
+1014	Sorry! Your session has expired
+1015	UserId/Password Required */
