b1c5534f04a374a70fce05ab631e1934181da942
Harish Gyanani <gyanani.harish@gmail.com>
2019-04-19 17:15:56 +0530

    Create colors.js

---
+++ res/colors.js
@@ -0,0 +1,72 @@
+export const GRADIENT_ARRAY = ['#0F3881', '#0E418A', '#0C4E97', '#0A5DA6', '#0967B0']
+export const TOOLBAR_ICON_SELECTED_BG = '#0E438C';
+
+//Text colors 
+export const TEXT_COLOR_WHITE = '#FFFFFF';
+export const TEXT_COLOR_BLUE='#097FC6';
+export const TEXT_COLOR_GREY='#4A4A4A'
+export const TEXT_COLOR_YELLOW = '#F4AF1D';
+export const TEXT_COLOR_DARK_BLUE='#067DC6';
+
+export const PRIMARY_COLOR_DARK_BLUE = '#094790';
+export const PRIMARY_COLOR_LIGHT = '#5F9CD6';
+export const PRIMARY_COLOR_BLUE = '#0A4484';
+export const ACCENT_COLOR_ORANGE = '#FE8803';
+
+export const CIRCLE_COLOR_YELLOW = '#F4AF1D';
+export const CHART_LINE_COLOR='#FB9304'
+export const PAGE_BG_COLOR_OFF_WHITE='#F6F6F6';
+
+
+//Simple colors
+export const WHITE='#FFF';
+export const BLACK='#000';
+export const TRANSPARENT_COLOR_CODE = '#00000000'
+export const LIGHT_GREY='#BBB'
+
+//Inbound
+export const COMPLETED_COLOR='#26BFB9';
+export const NOT_COMPLETED_COLOR='#D30E26';
+export const PENDING_COLOR='#FE8803';
+export const SUPPLIER_NAME_COLOR='#067DC6';
+
+
+export const ORDER_NO='#067DC6'
+export const NPF_ORDER='#067DC6'
+export const CARRIER='#4A4A4A'
+export const DELIVERY_IN_PROGRESS_COLOR='#F9B11B';
+export const DELIVERY_COMPLETE='#21BCB7';
+
+
+export const RESEND_COLOR='#067DC6';
+export const TRACK_THIS_ORDER='#FF9900';
+export const POD_REQUIRED='#1CBBB6';
+export const REPORT_ERROR='#E82941';
+
+export const ITEM_CODE_VALUE='#4A4A4A'
+export const DESCRIPTION_VALUE='#656565'
+export const QTY_ORDER_VALUE='#656565'
+export const OTY_SHIPPED_VALUE='#656565'
+
+//Report Packing color
+
+export const SELECT_ITEM_COLOR='#9B9B9B';
+
+//border color
+export const BORDER_COLOR_DARK_GREY='#343434'
+export const BORDER_COLOR_LIGHT_GREY='#DDD'
+export const BORDER_COLOR_LIGHT_BLUE='#4D83B8'
+
+//btn colors
+export const BTN_ORANGE='#FF9900';
+export const BTN_COLOR_PINK='#E82941'
+
+
+
+// TOP 5 SKU POP UP
+export const Header_background_top5sku='#A9A9A9'
+
+
+
+export const TINT_ARROW_GREEN='#6FC130'
+
